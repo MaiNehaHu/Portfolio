19baa3324c811d5ce9b7c782fb2097d25d2981c6
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2024-06-20 08:40:37 +0530

    change: exp

--- src/Componentss/Experience/ExperienceList.js
+++ src/Componentss/Experience/ExperienceList.js
@@ -5,22 +5,6 @@ import ajastos from '../../images/ajastos.png'
 
 export default [
   {
-    role: "UX/UI Intern",
-    company: "KaizIQ",
-    duration: "Oct, 2023 - Jan, 2024",
-    location: "Remote",
-
-    imageLink: kaizIQ,
-    link: "https://kaiziq.io/",
-    companyDescription:
-      "KaizIQ is a pioneering force in the realm of DevOps, redefining the way software development and operations are orchestrated. The mission is to empower organizations, irrespective of their scale, with a unified and seamlessly integrated DevOps ecosystem.",
-    responsibility: [
-      "Worked on setting up multi-screen responsiveness of KaizIQ's product interface for 8 pages and 1 major component in React JS and SCSS format.",
-      "Also, I helped set up multi-language input that gets stored in an SQL database.",
-    ],
-    tag: "internship"
-  },
-  {
     role: "React JS Developer Intern",
     company: "QuadB Tech",
     duration: "Feb, 2024 - Present",
@@ -33,6 +17,22 @@ export default [
     responsibility: [
       "Contributing to the development of Web3 Decentralized Apps within the ICP ecosystem using React JS.",
       "Collaborating on projects from ideation to launch, focusing on creating innovative solutions that bring visions to reality."
+    ],
+    tag: "internship"
+  },
+  {
+    role: "UX/UI Intern",
+    company: "KaizIQ",
+    duration: "Oct, 2023 - Jan, 2024",
+    location: "Remote",
+
+    imageLink: kaizIQ,
+    link: "https://kaiziq.io/",
+    companyDescription:
+      "KaizIQ is a pioneering force in the realm of DevOps, redefining the way software development and operations are orchestrated. The mission is to empower organizations, irrespective of their scale, with a unified and seamlessly integrated DevOps ecosystem.",
+    responsibility: [
+      "Worked on setting up multi-screen responsiveness of KaizIQ's product interface for 8 pages and 1 major component in React JS and SCSS format.",
+      "Also, I helped set up multi-language input that gets stored in an SQL database.",
     ],
     tag: "internship"
   },
